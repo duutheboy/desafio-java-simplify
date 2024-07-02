5ef58886f3a369faf810735b27e80e052e2f2a93
duutheboy <eduardobezerra777@outlook.com>
2024-07-02 20:17:02 -0300

    initial commit

---
+++ desafio-simplify/src/test/java/br/com/eduardobezerra/desafio_simplify/DesafioSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardobezerra.desafio_simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
